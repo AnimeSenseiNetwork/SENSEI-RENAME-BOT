a9e8b5544fa8858acba722161f111925461b57df
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-07-15 20:26:33 +0530

    config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://te.legra.ph/file/86e958f9fc0d7cbdf1a28.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '2036803347').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001936280485") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001936280485"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001934076980"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
